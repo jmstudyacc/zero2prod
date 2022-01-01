7bd4d3262a2e9e169f6c2b39d40bb4f207a7eaba
James <53650971+jmstudyacc@users.noreply.github.com>
2022-01-01 21:08:55 +0000

    adding health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World!");
     format!("Hello {}", name)
 }
 
+/// Responder is a conversion trait into an HttpResponse type
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    // .finish() sets an empty body & builds the HttpResponse
+    // impl Responder enables us to drop the .finish()
+    HttpResponse::Ok() //.finish()
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            // Add route for the root path registration
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            /* Add route for the greet handler registration
+            .route("/{name}", web::get().to(greet))*/
+            // Add route for the health check handler registration
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
